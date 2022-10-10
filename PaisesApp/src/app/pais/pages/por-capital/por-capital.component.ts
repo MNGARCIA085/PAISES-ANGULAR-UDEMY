a325b69496fd45cc3598c5358a759147fe8e7d1e
Marcos Nicolas Garcia Durand <mngarcia085@gmail.com>
2022-10-10 17:38:01 -0500

    por capital

--- PaisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
+++ PaisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  termino:string ='';
+ hayError: boolean = false;
+ paises: Country[] = [];
+ 
+ // inyecto el servicio
+ constructor( private paisService: PaisService ){};
+ 
+ 
+ sugerencias(termino:string){
+   this.hayError = false;
+   // TODO CREAR SUGERENCIAS
+ }
+ 
+ 
+ buscar(termino: string){
+   this.hayError = false; // quita el error anterior de haberlo
+   this.termino = termino;
+ 
+   // uso el servicio de busqueda
+   this.paisService.buscarCapital(this.termino).subscribe( (paises) => {
+     console.log(paises);
+     this.paises = paises;
+   }, (err) => {
+     console.log('Error');
+     this.hayError = true;
+     this.paises = []; // si hay error países será un arreglo vacío
+   });
+ }
 
 }
--- PaisesApp/src/app/pais/services/pais.service.ts
+++ PaisesApp/src/app/pais/services/pais.service.ts
@@ -17,12 +17,17 @@ export class PaisService {
   // de momento retorno any
   buscarPais( termino: string): Observable<Country[]> { 
     const url = `${ this.apiUrl }/name/${termino}`;
- 
- 
     // no uso .subscribe() porque no quiero retornar la info. dentro del servicio
     // sino a quien quiera que mando a llamar a buscarPais
     return this.http.get<Country[]>(url);
- 
   }
+
+
+  buscarCapital( termino: string): Observable<Country[]> { 
+    const url = `${ this.apiUrl }/capital/${termino}`;
+    return this.http.get<Country[]>(url);
+  }
+
+
  
 }
