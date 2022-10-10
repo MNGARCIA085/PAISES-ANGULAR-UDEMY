220181e1dc20e3c1b9e2ebe73ee2efb81c7bc857
Marcos Nicolas Garcia Durand <mngarcia085@gmail.com>
2022-10-09 22:47:26 -0500

    servicio y completado de la tabla

--- PaisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ PaisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
  
 @Component({
-  selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+ selector: 'app-por-pais',
+ templateUrl: './por-pais.component.html',
+ styles: [
+ ]
 })
 export class PorPaisComponent {
  
-  constructor() { }
+ termino:string ='';
+ hayError: boolean = false;
+ paises: Country[] = []; // creamos una variable del tipo arreglo de países que
+                         // vamos a ir completando
  
-  termino:string ='hola mundo';
+ // inyecto el servicio
+ constructor( private paisService: PaisService ){};
  
-  buscar(){
-    console.log(this.termino);
-  }
- 
- 
- 
+ buscar(){
+   this.hayError = false; // quita el error anterior de haberlo
+   console.log(this.termino);
+   // uso el servicio de busqueda
+   this.paisService.buscarPais(this.termino).subscribe( (paises) => {
+     console.log(paises);
+     this.paises = paises; // completo con los países
+   }, (err) => {
+     console.log('Error');
+     this.hayError = true;
+     this.paises = []; // si hay error países será un arreglo vacío
+   });
+ }
 }
---
+++ PaisesApp/src/app/pais/services/pais.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/pais-interface';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+ 
+  private apiUrl: string = "https://restcountries.com/v3.1";
+ 
+  // inyecto el servicio
+  constructor( private http: HttpClient ) { }
+ 
+  // se que voy a devolver un arreglo de paises, pero como todavia no se bien como es
+  // de momento retorno any
+  buscarPais( termino: string): Observable<Country[]> { 
+    const url = `${ this.apiUrl }/name/${termino}`;
+ 
+ 
+    // no uso .subscribe() porque no quiero retornar la info. dentro del servicio
+    // sino a quien quiera que mando a llamar a buscarPais
+    return this.http.get<Country[]>(url);
+ 
+  }
+ 
+}
