c32f283afc008690e8d3beb595eab84e61c618ba
Marcos Nicolas Garcia Durand <mngarcia085@gmail.com>
2022-10-09 23:34:49 -0500

    debounce time

--- PaisesApp/src/app/pais/components/pais-input/pais-input.component.ts
+++ PaisesApp/src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
  
 @Component({
  selector: 'app-pais-input',
@@ -6,19 +7,44 @@ import { Component, EventEmitter, Output } from '@angular/core';
  styles: [
  ]
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit { // necesito el OnInit
  
-  // para poder emitir termino
-  @Output() onEnter: EventEmitter<string> = new EventEmitter(); // se emite termino que es
-                                                              // de tipo string
-  
-  
-  termino: string = '';
-  
-  buscar(){
-    // emito
-    this.onEnter.emit(this.termino);
-  }
+ @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter(); // emito un string
+ 
+ debouncer: Subject<string> = new Subject(); // Subject permite crear un observable manualmente
+       // la idea es poder usar RxJs
+ 
+ // queremos que el debouncer se emita cuando dejo de escribir
+ 
+ // implemento el OnInit
+ // se dispara una única vez cuando el componente es creado
+ ngOnInit() {
+   this.debouncer.
+       pipe(debounceTime(300)).subscribe( valor => { // valor es el término de búsqueda
+       //console.log('debouncer:',valor);
+       this.onDebounce.emit(valor);
+   })
+   // debounceTime(300) básicamente dice no emitas el suscribe hasta que el obsevable
+   // deje de emitir valores por los próximos 300ms
+ }
+ 
+ 
+ termino: string = '';
+ 
+ buscar(){
+   // emito
+   this.onEnter.emit(this.termino);
+ }
+ 
+ teclaPresionada(event: any) {
+   this.debouncer.next(this.termino); // llamo al debouncer y emito un valor
+      // el siguiente valor que emito es this.termino
+      // el next esta suscrito arriba en "this.debouncer.subscribe"
+      // asi cuando alguien presiona una tecla manda a llamar al next que
+      // esta suscrito y hace lo que corresponda en ese parte el código
   
  }
  
+ 
+}
--- PaisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ PaisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -12,26 +12,30 @@ export class PorPaisComponent {
  
  termino:string ='';
  hayError: boolean = false;
- paises: Country[] = []; // creamos una variable del tipo arreglo de países que
-                         // vamos a ir completando
+ paises: Country[] = [];
  
-  // inyecto el servicio
-  constructor( private paisService: PaisService ){};
+ // inyecto el servicio
+ constructor( private paisService: PaisService ){};
  
-  buscar(termino: string){
-    this.hayError = false; // quita el error anterior de haberlo
-    this.termino = termino;
-
-    // uso el servicio de busqueda
-    this.paisService.buscarPais(this.termino).subscribe( (paises) => {
-      console.log(paises);
-      this.paises = paises;
-    }, (err) => {
-      console.log('Error');
-      this.hayError = true;
-      this.paises = []; // si hay error países será un arreglo vacío
-    });
-  }
+ 
+ sugerencias(termino:string){
+   this.hayError = false;
+   // TODO CREAR SUGERENCIAS
+ }
+ 
+ 
+ buscar(termino: string){
+   this.hayError = false; // quita el error anterior de haberlo
+   this.termino = termino;
+ 
+   // uso el servicio de busqueda
+   this.paisService.buscarPais(this.termino).subscribe( (paises) => {
+     console.log(paises);
+     this.paises = paises;
+   }, (err) => {
+     console.log('Error');
+     this.hayError = true;
+     this.paises = []; // si hay error países será un arreglo vacío
+   });
+ }
 }
-
-
