85610a649d16308370f5c174703aa654ec1690c7
Marcos Nicolas Garcia Durand <mngarcia085@gmail.com>
2022-10-10 22:29:58 -0500

    sugerencias

--- PaisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ PaisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,7 +5,11 @@ import { PaisService } from '../../services/pais.service';
 @Component({
  selector: 'app-por-pais',
  templateUrl: './por-pais.component.html',
- styles: [
+ styles: [ `
+    li{
+      cursor:pointer
+    }  
+  `
  ]
 })
 export class PorPaisComponent {
@@ -13,15 +17,31 @@ export class PorPaisComponent {
  termino:string ='';
  hayError: boolean = false;
  paises: Country[] = [];
+ paisesSugeridos: Country[] = [];
+ mostrarSugerencias: boolean = false;
  
  // inyecto el servicio
  constructor( private paisService: PaisService ){};
  
  
- sugerencias(termino:string){
-   this.hayError = false;
-   // TODO CREAR SUGERENCIAS
- }
+ sugerencias( termino: string ) {
+  this.hayError = false;
+  this.termino = termino;
+  this.mostrarSugerencias = true;
+  
+  this.paisService.buscarPais( termino )
+    .subscribe( 
+      paises => this.paisesSugeridos = paises.splice(0,5),
+      (err) => this.paisesSugeridos = []
+    );
+
+    console.log(this.paisesSugeridos);
+
+}
+
+buscarSugerido( termino: string ) {
+  this.buscar( termino );
+}
  
  
  buscar(termino: string){
@@ -38,4 +58,7 @@ export class PorPaisComponent {
      this.paises = []; // si hay error países será un arreglo vacío
    });
  }
+
+
+
 }
