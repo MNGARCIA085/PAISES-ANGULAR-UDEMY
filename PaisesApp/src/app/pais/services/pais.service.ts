7090bf36d9aa9fdca4eaccbbdc53cdda3de1e27d
Marcos Nicolas Garcia Durand <mngarcia085@gmail.com>
2022-10-10 20:05:49 -0500

    optimizar peticiones http

--- PaisesApp/src/app/pais/services/pais.service.ts
+++ PaisesApp/src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais-interface';
@@ -9,6 +9,12 @@ import { Country } from '../interfaces/pais-interface';
 export class PaisService {
  
   private apiUrl: string = "https://restcountries.com/v3.1";
+
+  get httpParams(){
+    return new HttpParams().set(
+      'fields','name,capital,cca2,flags,population'
+    )
+  }
  
   // inyecto el servicio
   constructor( private http: HttpClient ) { }
@@ -19,13 +25,13 @@ export class PaisService {
     const url = `${ this.apiUrl }/name/${termino}`;
     // no uso .subscribe() porque no quiero retornar la info. dentro del servicio
     // sino a quien quiera que mando a llamar a buscarPais
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams});
   }
 
   // buscar por capital
   buscarCapital( termino: string): Observable<Country[]> { 
     const url = `${ this.apiUrl }/capital/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams});
   }
 
   // obtener datos de un pa[is
@@ -37,7 +43,7 @@ export class PaisService {
   // paises por region
   getPaisPorRegion( region: string): Observable<Country[]> { 
     const url = `${ this.apiUrl }/region/${region}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams});
   }
 
 
